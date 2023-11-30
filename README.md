2c98fb64fb80058ceaecc4f54ca1d727e6074686
phawitpp <phawit.monc@kmutt.ac.th>
2023-11-30 15:38:28 +0700

    Change landmine after sound

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# LethalCompanyTemplate
-A template for a BepInEx 5 Plugin, using the correct .csproj configuration for Lethal Company modding
+# SuperLandmine
+
+Add more power to landmine
+
+* Landmine is spawn more oftenly
+* Landmine idle sound is disabled
+* Landmine damage and kill range is increase
+* Play "to be continue" song when you trigger the landmine
+
+## Installation
+** Require BepInEx installed first
+
+1. Via [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager) or [r2modman](https://for-the-king.thunderstore.io/package/ebkr/r2modman/)
+
+2. Manually download and copy plugins folder and paste in BepInEx folder 
+
+
+
+## Changelog
+
+### 1.0.0
+
+* Initial release
+
+
+## Reporting bugs
+You can issue the bugs in the github repository
--- SuperLandmine/Patchs/SuperLandminePatch.cs
+++ SuperLandmine/Patchs/SuperLandminePatch.cs
@@ -16,10 +16,10 @@ namespace SuperLandmine.Patchs
         public static void playAtomicSound(Landmine __instance)
         {
 
-            Plugin.log.LogInfo(">>>>>>>>>>Play atomic sound");
+            Plugin.log.LogInfo(">>>>>>>>>>Play continue sound");
             __instance.mineAudio.volume = 1.5f;
             __instance.mineFarAudio.volume = 1.5f;
-            string path = "file://" + Paths.PluginPath + "\\SuperLandmine\\atomic.mp3";
+            string path = "file://" + Paths.PluginPath + "\\SuperLandmine\\continue.mp3";
             UnityWebRequest audioClip = UnityWebRequestMultimedia.GetAudioClip(path, (AudioType)13);
             audioClip.SendWebRequest();
             while (!audioClip.isDone)
@@ -40,13 +40,15 @@ namespace SuperLandmine.Patchs
 
         private static IEnumerator DelayedActions(Landmine __instance)
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(5);
             __instance.mineAudio.volume = 1.5f;
             __instance.mineFarAudio.volume = 1.5f;
             __instance.mineAudio.pitch = Random.Range(0.93f, 1.2f);
             __instance.mineAudio.PlayOneShot(__instance.mineDetonate, 1.5f);
             Landmine.SpawnExplosion(__instance.transform.position + Vector3.up, spawnExplosionEffect: true, 10.0f, 12.0f);
             yield return new WaitForSeconds(2);
+            __instance.mineAudio.volume = 0f;
+            __instance.mineFarAudio.volume = 0f;
         }
 
         [HarmonyPatch("StartIdleAnimation")]
